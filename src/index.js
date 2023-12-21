ba5002839bbe4574640591a5f302d66460861fb6
Oleksandr Kharutsa <al.haruca@gmail.com>
2023-12-21 15:25:34 +0100

    fix App import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import App from './App';
+import { App } from './App';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
